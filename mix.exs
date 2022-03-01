92b826c8f7eeb156569611fd92ef99a719b93747
Guido Van Nyvel <guido.vn@hotmail.com>
2022-03-01 14:25:21 +0100

    Add Skitter and npm

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule DashboardSkitter.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:skitter, github: "mathsaey/skitter"},
     ]
   end
 
